1efc7cc44ea0d19de1e1a0d56c8f49a6daf09242
daudonhere <hellodadedaud@gmail.com>
2025-02-15 16:40:35 +0700

    change user routes

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,10 +1,12 @@
 import express from "express";
-import { fetchUserData, updateUserData } from "@/controllers/userController";
+import { UserController } from "@/controllers/userController";
 import { authMiddleware } from "@/middlewares/authMiddleware";
+import { UserRepository } from "@/repositories/userCollection";
 
 const router = express.Router();
+const userController = new UserController(new UserRepository());
 
-router.get("/user/:id", authMiddleware, fetchUserData);
-router.put("/user/:id", authMiddleware, updateUserData);
+router.get("/users/:id", authMiddleware, userController.fetchUserData.bind(userController));
+router.put("/users/:id", authMiddleware, userController.updateUserData.bind(userController));
 
 export default router;
